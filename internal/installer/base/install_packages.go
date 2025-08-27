8205093c855e8ea5efc60e68e364883086008b15
AnvilM <211anvil112@gmail.com>
2025-08-27 18:04:29 +0300

    feat: add configs

--- internal/installer/base/install_packages.go
+++ internal/installer/base/install_packages.go
@@ -1,37 +1,53 @@
 package base
 
 import (
-	"fmt"
-	"time"
-
 	"anvilarch/internal/utils/logger"
 	"anvilarch/internal/utils/system"
+	_ "embed"
+	"fmt"
+	"log"
+	"time"
 
 	"github.com/briandowns/spinner"
+	"gopkg.in/yaml.v3"
 )
 
+//go:embed config.yml
+var packagesYAML []byte
+
+type Package struct {
+    Name          string   `yaml:"name"`
+    SetupCommands []string `yaml:"setupCommands,omitempty"`
+}
+
 func InstallPackages() {
-	allPkgs := []string{
-		"cliphist", "fish", "grim", "hyprland", "kitty", "less",
-		"networkmanager", "pipewire-pulse", "rofi", "slurp", "swww",
-		"xdg-desktop-portal-hyprland", "waybar", "wireplumber",
-		"noto-fonts-emoji", "ttf-jetbrains-mono", "ttf-nerd-fonts-symbols-mono",
-		"ttf-roboto",
-	}
+
+	var pkgs []Package
+    if err := yaml.Unmarshal(packagesYAML, &pkgs); err != nil {
+        log.Fatal(err)
+    }
 
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.Start()
-	for _, pkg := range allPkgs {
-		s.Suffix = fmt.Sprintf(" Installing packages: %s", pkg)
-		
-		if err := system.RunCommand("pacman", "-S", "--noconfirm", pkg); err != nil {
+
+    for _, p := range pkgs {
+
+        s.Suffix = fmt.Sprintf(" Installing packages: %s", p.Name)
+
+		if err := system.RunCommand("pacman -S " + p.Name); err != nil {
 			s.Stop()
-			logger.Fatal("%s: %v", pkg, err)
+			logger.Fatal("%s", err)
 		}
-		
-	}
+
+        for _, cmd := range p.SetupCommands {
+			if err := system.RunCommand(cmd); err != nil {
+				s.Stop()
+				logger.Fatal("%s", err)
+			}
+        }
+    }
+
 	s.Stop()
 
 	logger.Log(logger.SUCCESS, "All packages successfully installed!")
-	
 }
