cc9f885e13626b5e8972f1027163e4780e9436fa
AnvilM <211anvil112@gmail.com>
2025-08-31 11:57:55 +0300

    a

--- cmd/installer/main.go
+++ cmd/installer/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"sprout/internal/installer"
-	"sprout/internal/utils/system"
 	"os/exec"
+	"fmt"
 )
 
 func main() {
 	command := "echo $SUDO_USER"
 	cmd := exec.Command("sh", "-c", command)
 
-	installer.Install()
+	out, err := cmd.CombinedOutput()
+
+	fmt.Printf("%s", out)
+	if(err != nil){
+		fmt.Printf("%s", err)
+	}
 }
